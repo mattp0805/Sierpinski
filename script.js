6db481ae323a8d210ab1df5f60f7ff1746049dda
matt <mattp0805@gmail.com>
2022-08-23 23:23:57 +0100

    established triangle class

--- script.js
+++ script.js
@@ -1,9 +1,116 @@
 class triangle{
 
     constructor(){
-            this.width = width;
-            this.height = height;
+            this.width = window.innerWidth;
+            this.height = window.innerHeight;
+            this.points = [];
+            this.vertices = {
+                x1: 0,
+                y1: 0,
+                x2: 0,
+                y2: 0,
+                x3: 0,
+                y3: 0
+            }
 
+    }
+
+    setTriangle(){
+        const cx = this.width / 2;
+        const cy = this.height / 2;
+        let shrink = 0.2;
+        let maxL = this.width;
+        while (maxL > this.height) {
+            maxL = maxL - shrink;
+            shrink += 0.05;
+        }
+        const L = maxL;
+        // R = height of triangle
+        const R = (L * Math.sqrt(3)) / 2;
+
+
+        //define triangle
+        this.vertices.x1 = cx - L / 2;
+        this.vertices.y1 = cy + R / 2;
+        this.vertices.x2 = this.vertices.x1 + L;
+        this.vertices.y2 = this.vertices.y1;
+        this.vertices.x3 = cx;
+        this.vertices.y3 = this.vertices.y2 - R;  
+        
+    }
+    
+    #randomPoint() {
+        var valid = false;
+        const minX = this.vertices.x1;
+        const maxX = this.vertices.x2;
+        const minY = this.vertices.y1;
+        let y = 0;
+        let x = Math.random() * (maxX - minX) + minX;
+        let leftOrRight = "l";
+        if (x > cx) {
+            leftOrRight = "r";
+        }
+        let maxY = 0;
+        if (leftOrRight == "l") {
+            maxY = minY - (x - minX) * Math.tan(Math.PI / 3);
+        } else {
+            maxY = minY - (maxX - x) * Math.tan(Math.PI / 3);
+        }
+    
+        while (valid == false) {
+            y = Math.random() * (minY - maxY) + maxY;
+            if (y > maxY) {
+                valid = true;
+            }
+        }
+        var coords = { x: x, y: y };
+        return coords;
+    }
+    
+    #findHalfway(currentPoint, destination) {
+        let halfwayPoint = { x: 0, y: 0 };
+        if (currentPoint.x < destination.x) {
+            halfwayPoint.x = currentPoint.x + (destination.x - currentPoint.x) / 2;
+        } else {
+            halfwayPoint.x = currentPoint.x - (currentPoint.x - destination.x) / 2;
+        }
+    
+        if (currentPoint.y < destination.y) {
+            halfwayPoint.y = currentPoint.y + (destination.y - currentPoint.y) / 2;
+        } else {
+            halfwayPoint.y = currentPoint.y - (currentPoint.y - destination.y) / 2;
+        }
+        return halfwayPoint;
+    }
+    
+    startChaosGame(r) {
+        let startCoords = this.#randomPoint();
+        let count = 0;
+        let currentPoint = { x: startCoords.x, y: startCoords.y};
+        let destination = { x: 0, y: 0 };
+        let halfway = null;
+    
+        while (count < r) {
+            let vertex = Math.floor(Math.random() * 3) + 1;
+            switch (vertex) {
+                case 1:
+                    destination.x = this.vertices.x1;
+                    destination.y = this.vertices.y1;
+                    break;
+                case 2:
+                    destination.x = this.vertices.x2;
+                    destination.y = this.vertices.y2;
+                    break;
+                case 3:
+                    destination.x = this.vertices.x3;
+                    destination.y = this.vertices.y3;
+                    break;
+            }
+            halfway = findHalfway(currentPoint, destination);
+            points.push(halfway);
+            currentPoint = halfway;
+            count++;
+        }
     }
 }
 
@@ -12,26 +119,9 @@ class triangle{
 console.log("start");
 //declare globals
 const canvas = document.getElementById("c"), scale = document.getElementById("scale");
-
 const ctx = canvas.getContext("2d");
-var points = [];
-var currPoint = null;
-let w = window.innerWidth;
-let h = window.innerHeight;
-canvas.height = h;
-canvas.width = w;
-var cx = w / 2;
-var cy = h / 2;
-var points = [];
-let triangle = {
-    x1: 0,
-    x2: 0,
-    x3: 0,
-    y1: 0,
-    y2: 0,
-    y3: 0
-};
-
+canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
 
 //adjustable globals
 let triLineWidth = 1;
@@ -61,116 +151,6 @@ function transformPoint(point = {x:0, y:0},newWindowWidth,newWindowHeight){
     newPoint.x = (point.x * newWindowWidth) / w;
     newPoint.y = (point.y * newWindowHeight) / h;
     return newPoint;
-}
-
-function setTriangle(){
-    w = window.innerWidth;
-    h = window.innerHeight;
-    cx = w / 2;
-    cy = h / 2;
-    let shrink = 0.2;
-    let maxL = w;
-    while (maxL > h) {
-        maxL = maxL - shrink;
-        shrink += 0.05;
-    }
-    let L = maxL;
-    
-    // R = height of triangle
-    let R = (L * Math.sqrt(3)) / 2;
-
-    //define triangle
-    triangle.x1 = cx - L / 2;
-    triangle.y1 = cy + R / 2;
-    triangle.x2 = triangle.x1 + L;
-    triangle.y2 = triangle.y1;
-    triangle.x3 = cx;
-    triangle.y3 = triangle.y2 - R;  
-}
-
-function drawTriangle() {
-    ctx.beginPath();
-    ctx.lineWidth = triLineWidth;
-    ctx.strokeStyle = dotColor;
-    ctx.moveTo(triangle.x1, triangle.y1);
-    ctx.lineTo(triangle.x2, triangle.y2);
-    ctx.lineTo(triangle.x3, triangle.y3);
-    ctx.lineTo(triangle.x1, triangle.y1);
-    ctx.stroke();
-    ctx.closePath();
-}
-
-function randomPoint() {
-    var valid = false;
-    const minX = triangle.x1;
-    const maxX = triangle.x2;
-    const minY = triangle.y1;
-    let y = 0;
-    let x = Math.random() * (maxX - minX) + minX;
-    let leftOrRight = "l";
-    if (x > cx) {
-        leftOrRight = "r";
-    }
-    let maxY = 0;
-    if (leftOrRight == "l") {
-        maxY = minY - (x - minX) * Math.tan(Math.PI / 3);
-    } else {
-        maxY = minY - (maxX - x) * Math.tan(Math.PI / 3);
-    }
-
-    while (valid == false) {
-        y = Math.random() * (minY - maxY) + maxY;
-        if (y > maxY) {
-            valid = true;
-        }
-    }
-    var coords = { x: x, y: y };
-    return coords;
-}
-
-function findHalfway(currentPoint, destination) {
-    let halfwayPoint = { x: 0, y: 0 };
-    if (currentPoint.x < destination.x) {
-        halfwayPoint.x = currentPoint.x + (destination.x - currentPoint.x) / 2;
-    } else {
-        halfwayPoint.x = currentPoint.x - (currentPoint.x - destination.x) / 2;
-    }
-
-    if (currentPoint.y < destination.y) {
-        halfwayPoint.y = currentPoint.y + (destination.y - currentPoint.y) / 2;
-    } else {
-        halfwayPoint.y = currentPoint.y - (currentPoint.y - destination.y) / 2;
-    }
-    return halfwayPoint;
-}
-
-function chaosGame(r, startX, startY) {
-    let count = 0;
-    let currentPoint = { x: startX, y: startY };
-    let destination = { x: 0, y: 0 };
-    let halfway = null;
-
-    while (count < r) {
-        let vertex = Math.floor(Math.random() * 3) + 1;
-        switch (vertex) {
-            case 1:
-                destination.x = triangle.x1;
-                destination.y = triangle.y1;
-                break;
-            case 2:
-                destination.x = triangle.x2;
-                destination.y = triangle.y2;
-                break;
-            case 3:
-                destination.x = triangle.x3;
-                destination.y = triangle.y3;
-                break;
-        }
-        halfway = findHalfway(currentPoint, destination);
-        points.push(halfway);
-        currentPoint = halfway;
-        count++;
-    }
 }
 
 function drawChaosGame(delay) {
@@ -203,9 +183,6 @@ function resetCanvas() {
 
 resetCanvas();
 
-
-
-let startCoords = randomPoint();
 setTriangle();
 drawTriangle();
 document.getElementsByClassName("panel-item start")[0].addEventListener("click", function(){
